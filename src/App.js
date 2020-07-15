c3c8b9bb797773c112921b7eb376e2d18bc8e059
Galo Sandoval <galosan92@gmail.com>
2020-07-15 14:29:33 -0700

    worked on body page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
-import React, { useState, useEffect} from "react";
+import React from "react";
 import "./App.css";
-import {API_KEY, BASE_URL} from './components/Constants.js'
-import axios from 'axios'
+
 import Body from './components/Body.js'
 
 
 function App() {
-  const [data, setData] = useState([])
-  const nasaData = () => {
-    setData(data.map(nasaData => {
-    return nasaData
-  }))
-}
-
-  useEffect(() => {
-    axios.get(`${BASE_URL}api_key=${API_KEY}`)
-    .then(res => {
-      console.log(res.data)
-      setData(res.data)
-      // useState(res.data)
-    })
-    .catch(err => {
-      debugger
-    })
-  }, [])
-
   return (
     <div className="App">
-        <Body key={nasaData.id} nasaData={nasaData}/>
+        <Body />
     
       <p>
         Read through the instructions in the README.md file to build your NASA
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import Header from './components/Header.js'
+import {API_KEY, BASE_URL} from './components/Constants.js'
+import axios from 'axios'
 
 const Body = (props) => {
-  const { nasaData } = props;
+  const [data, setData] = useState([])
+  const nasaData = () => {
+    setData(data.map(nasaData => {
+    return nasaData
+  }))
+}
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}api_key=${API_KEY}`)
+    .then(res => {
+      console.log(res.data)
+      setData(res.data)
+      // useState(res.data)
+    })
+    .catch(err => {
+      debugger
+    })
+  }, [])
   return (
     <div className="title">
       <Header key={nasaData.title} />
