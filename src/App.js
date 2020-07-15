b5276b245337ed6ab535b0ade6fd74fe954d7232
Galo Sandoval <galosan92@gmail.com>
2020-07-15 14:19:05 -0700

    made files for components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, { useState, useEffect} from "react";
 import "./App.css";
 import {API_KEY, BASE_URL} from './components/Constants.js'
 import axios from 'axios'
-import Body from '.components/Body.js'
+import Body from './components/Body.js'
 
 
 function App() {
   const [data, setData] = useState([])
+  const nasaData = () => {
+    setData(data.map(nasaData => {
+    return nasaData
+  }))
+}
 
   useEffect(() => {
     axios.get(`${BASE_URL}api_key=${API_KEY}`)
@@ -22,9 +27,8 @@ function App() {
 
   return (
     <div className="App">
-      {data.map(p => {
-        <Body key={p.id} propsPassedFromParent = {p}/>
-      })}
+        <Body key={nasaData.id} nasaData={nasaData}/>
+    
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import Header from './components/Header.js'
 
-const Body = () => {
-  return <div />
+const Body = (props) => {
+  const { nasaData } = props;
+  return (
+    <div className="title">
+      <Header key={nasaData.title} />
+      
+    </div>
+  )
+
 }
 
 export default Body;
